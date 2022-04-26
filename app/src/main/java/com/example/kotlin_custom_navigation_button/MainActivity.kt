1ecfdfe62e8d6734f0472db60039de60a6742708
majid2851 <majidbagheri2851@gmail.com>
2022-04-26 12:43:38 +0430

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_custom_navigation_button/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.kotlin_custom_navigation_button
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/* https://github.com/ismaeldivita/chip-navigation-bar*/
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
